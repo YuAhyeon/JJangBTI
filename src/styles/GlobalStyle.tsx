0dfc1575d0f1a0709d1aa4b0a229f2099f3b2ba3
youa7878 <dkgus7123@naver.com>
2023-05-25 18:10:51 +0900

    ✨ Create GlobalStyle - #1

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+
+  html, body {
+    font-size: var(--font-size-m);
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    cursor: pointer;
+    padding: 0;
+    border: none;
+    outline: none;
+    background-color: transparent;
+  }
+  
+  input {
+    border: none;
+    outline: none;
+  }
+`;
+
+export default GlobalStyle;
