7cd98be9b70af27c21b3289467e7bd9ff4bc6ea0
youa7878 <dkgus7123@naver.com>
2023-05-28 00:31:58 +0900

    💄 font 적용 - #1

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,6 +1,7 @@
 import reset from 'styled-reset';
 import variables from './variables';
 import { createGlobalStyle } from 'styled-components';
+import NanumJangMiCe from '../assets/fonts/NanumJangMiCe.ttf';
 
 const GlobalStyle = createGlobalStyle`
   ${reset};
@@ -9,11 +10,17 @@ const GlobalStyle = createGlobalStyle`
   *, *:before, *:after {
     box-sizing: border-box;
   }
-
+  
+  @font-face {
+  font-family: 'rose';
+  src: url(${NanumJangMiCe}) format('truetype');
+  }
+  
   html, body {
     background-color: var(--light-pink);
     background-image: url('https://velog.velcdn.com/images/youa7878/post/59da6e5f-c4c2-4a52-a347-468b019f9a4b/image.png');
     background-repeat : repeat;
+    font-family: 'rose';
   }
 
   a {
@@ -33,6 +40,7 @@ const GlobalStyle = createGlobalStyle`
     border: none;
     outline: none;
   }
+  
 `;
 
 export default GlobalStyle;
