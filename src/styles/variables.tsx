0bf3c277c34aa77463573e05be34768e71386b46
youa7878 <dkgus7123@naver.com>
2023-05-27 21:50:21 +0900

    ✨ variables 생성 - #1

---
+++ src/styles/variables.tsx
@@ -0,0 +1,13 @@
+import { css } from 'styled-components';
+
+const variables = css`
+  :root {
+    --pink: #fa5d65;
+    --black: #232629;
+    --yellow: #ffce45;
+    --light-pink: #ffd1d3;
+    --light-green: #e9fbe6;
+  }
+`;
+
+export default variables;
