6cb71a5ec4e64be3624dc67bbfae1da112509fe3
youa7878 <dkgus7123@naver.com>
2023-05-29 00:03:31 +0900

    🔨 Question Image, Button 추가 - #12

--- src/components/Question/QuestionContent.tsx
+++ src/components/Question/QuestionContent.tsx
@@ -1,18 +1,41 @@
+import { styled } from 'styled-components';
 import Title from '../common/Title';
 import ProgressBar from '../common/ProgressBar';
 import { Container, Subtitle } from '../../styles/shared';
+import QuestionImageSrc from '../../assets/question-images/Question1.png';
+import buttonCSS from '../common/Button';
 
 function QuestionContent() {
   return (
     <Container>
       <Title />
       <ProgressBar value={8} />
-      <Subtitle>
-        산책하던 중 떡잎공원에서 <br />
-        놀고 있는 떡잎마을 방범대가 보인다!
-      </Subtitle>
+      <Subtitle>질문</Subtitle>
+      <QuestionImage src={QuestionImageSrc} />
+      <Button aria-label="답변 버튼" type="button">
+        1
+      </Button>
+      <Button aria-label="답변 버튼" type="button">
+        2
+      </Button>
     </Container>
   );
 }
+
+const QuestionImage = styled.img`
+  width: 100%;
+  height: 150px;
+  border-radius: 3px;
+  margin-bottom: 30px;
+`;
+
+const Button = styled.button`
+  ${buttonCSS};
+  height: 80px;
+  margin: 10px 0;
+  font-size: 22px;
+  color: #ffffff;
+  letter-spacing: 1px;
+`;
 
 export default QuestionContent;
