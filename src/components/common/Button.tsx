4925b001df20b44dcb82b5f2fa2ec39ae7023818
youa7878 <dkgus7123@naver.com>
2023-05-28 18:23:09 +0900

    💄 버튼 컴포넌트 style 수정 - #2

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -5,12 +5,28 @@ function Button() {
 }
 
 const ButtonBox = styled.button`
-  background-color: var(--yellow);
   width: 100%;
   height: 60px;
-  border-radius: 5px;
-  border: 0;
-  box-shadow: 0px 4px 0px #eebc31;
+
+  background-color: #f7d53e;
+  background: linear-gradient(to bottom, #f7d53e 5%, #ffca38 100%);
+
+  border-radius: 6px;
+  border: 1px solid #f5b812;
+  box-shadow: inset 0px 1px 0px 0px #fce2c1;
+
+  padding: 6px 24px;
+  text-shadow: 0px 1px 0px #f7cd5c;
+
+  &:hover {
+    background: linear-gradient(to bottom, #ffca38 5%, #f7d53e 100%);
+    background-color: #ffca38;
+  }
+
+  &:active {
+    position: relative;
+    top: 1px;
+  }
 `;
 
 export default Button;
