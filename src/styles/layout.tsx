34af95f32da691271e8a0cf1deb6b05b2f02a807
youa7878 <dkgus7123@naver.com>
2023-05-28 18:24:26 +0900

    💄 layout style 수정 - #1

--- src/styles/layout.tsx
+++ src/styles/layout.tsx
@@ -4,9 +4,7 @@ import { colFlexCenter } from './shared';
 const MainContainer = styled.main`
   ${colFlexCenter};
   margin: auto;
-  height: 100vh;
   max-width: 500px;
-  background-color: var(--pink);
 `;
 
 export default MainContainer;
