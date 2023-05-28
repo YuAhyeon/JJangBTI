39ceebe13f8d4be74264541564cf203e144ade10
youa7878 <dkgus7123@naver.com>
2023-05-29 01:10:50 +0900

    🔨 Add ResultContent - #13

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,10 +1,12 @@
 import Logo from '../components/common/Logo';
 import MainContainer from '../styles/layout';
+import ResultContent from '../components/Result/ResultContent';
 
 function Result() {
   return (
     <MainContainer>
       <Logo />
+      <ResultContent />
     </MainContainer>
   );
 }
