10d1719f6e3e739f40eabbc742d6de4a450dc404
youa7878 <dkgus7123@naver.com>
2023-05-28 21:24:22 +0900

    🔨 Add Title, Subtitle - #12

--- src/components/Question/QuestionContent.tsx
+++ src/components/Question/QuestionContent.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import Title from '../common/Title';
+import { Container, Subtitle } from '../../styles/shared';
 
 function QuestionContent() {
-  return <div></div>;
+  return (
+    <Container>
+      <Title />
+      <Subtitle>
+        산책하던 중 떡잎공원에서 <br />
+        놀고 있는 떡잎마을 방범대가 보인다!
+      </Subtitle>
+    </Container>
+  );
 }
 
 export default QuestionContent;
