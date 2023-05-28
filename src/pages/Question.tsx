ff6183811309d3f50514b78215ac4bb170d7442d
youa7878 <dkgus7123@naver.com>
2023-05-28 21:09:18 +0900

    🔨 Add QuestionContent - #12

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -1,11 +1,12 @@
-import styled from 'styled-components';
 import Logo from '../components/common/Logo';
 import MainContainer from '../styles/layout';
+import QuestionContent from '../components/Question/QuestionContent';
 
 function Question() {
   return (
     <MainContainer>
       <Logo />
+      <QuestionContent />
     </MainContainer>
   );
 }
