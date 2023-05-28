8099da12ed6fd9a26675849ce90b34b83019115e
youa7878 <dkgus7123@naver.com>
2023-05-28 20:28:02 +0900

    🔨 export문 수정 - #1

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import GlobalStyle from '../styles/GlobalStyle';
 
-export function Root() {
+export default function Root() {
   return (
     <>
       <GlobalStyle />
