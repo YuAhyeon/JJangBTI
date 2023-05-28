6ed63650de2f7b83b31032224e31d3c34e381b9f
youa7878 <dkgus7123@naver.com>
2023-05-28 22:09:39 +0900

    🔨 Add ProgressBar - #12

--- src/components/Question/QuestionContent.tsx
+++ src/components/Question/QuestionContent.tsx
@@ -1,10 +1,12 @@
 import Title from '../common/Title';
+import ProgressBar from '../common/ProgressBar';
 import { Container, Subtitle } from '../../styles/shared';
 
 function QuestionContent() {
   return (
     <Container>
       <Title />
+      <ProgressBar value={8} />
       <Subtitle>
         산책하던 중 떡잎공원에서 <br />
         놀고 있는 떡잎마을 방범대가 보인다!
