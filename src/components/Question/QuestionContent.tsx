1a863ea743edb16ae406958903c4d5fced720099
youa7878 <dkgus7123@naver.com>
2023-05-29 20:59:44 +0900

    🔨 questionData 적용 - #18

--- src/components/Question/QuestionContent.tsx
+++ src/components/Question/QuestionContent.tsx
@@ -1,25 +1,85 @@
+import { useState } from 'react';
 import { styled } from 'styled-components';
-import Title from '../common/Title';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import buttonCSS from '../common/Button';
 import ProgressBar from '../common/ProgressBar';
 import { Container, Subtitle } from '../../styles/shared';
-import QuestionImageSrc from '../../assets/question-images/Question1.png';
-import buttonCSS from '../common/Button';
 
-function QuestionContent() {
-  return (
-    <Container>
-      <Title />
-      <ProgressBar value={8} />
-      <Subtitle>질문</Subtitle>
-      <QuestionImage src={QuestionImageSrc} />
-      <Button aria-label="답변 버튼" type="button">
-        1
-      </Button>
-      <Button aria-label="답변 버튼" type="button">
-        2
-      </Button>
-    </Container>
-  );
+interface TotalScore {
+  EI: number;
+  SN: number;
+  TF: number;
+  JP: number;
+  [key: string]: number;
+}
+
+interface Question {
+  id: number;
+  title: string;
+  image: string;
+  answerA: string;
+  answerB: string;
+  gauge: number;
+  type: string;
+}
+
+interface QuestionData {
+  questionData: Question;
+}
+
+function QuestionContent({ questionData }: QuestionData) {
+  const { questionId } = useParams() as { questionId: string };
+  const [totalScore, setTotalScore] = useState<TotalScore>({
+    EI: 0,
+    SN: 0,
+    TF: 0,
+    JP: 0,
+  });
+  const navigate = useNavigate();
+
+  console.log(totalScore);
+
+  const handleClick = (buttonType: string) => {
+    if (buttonType === 'A') {
+      console.log(questionData.type);
+      setTotalScore((prev) => ({
+        ...prev,
+        [questionData.type]: prev[questionData.type] + 1,
+      }));
+    }
+
+    if (questionData.id === 12) {
+      navigate(`/result`);
+    } else {
+      navigate(`/question/${String(questionData.id + 1)}`);
+    }
+  };
+
+  if (Number(questionId) === questionData.id) {
+    return (
+      <Container>
+        <ProgressBar value={(questionData.id / 12) * 100} />
+        <Subtitle>{questionData.title}</Subtitle>
+        <QuestionImage src={questionData.image} />
+        <Button
+          aria-label="답변 A 버튼"
+          type="button"
+          onClick={() => handleClick('A')}
+        >
+          {questionData.answerA}
+        </Button>
+        <Button
+          aria-label="답변 B 버튼"
+          type="button"
+          onClick={() => handleClick('B')}
+        >
+          {questionData.answerB}
+        </Button>
+      </Container>
+    );
+  }
+  return null;
 }
 
 const QuestionImage = styled.img`
@@ -33,9 +93,10 @@ const Button = styled.button`
   ${buttonCSS};
   height: 80px;
   margin: 10px 0;
-  font-size: 23px;
+  font-size: 21px;
   color: #ffffff;
   letter-spacing: 1.5px;
+  font-family: 'junghagsaeng';
 `;
 
 export default QuestionContent;
