d7b597739b8b14b20518ecba504245aaa41f4d6f
youa7878 <dkgus7123@naver.com>
2023-05-30 18:21:36 +0900

    🔨 Add resetTotalScore - #2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
+import { useResetRecoilState } from 'recoil';
 import Logo from '../components/common/Logo';
 import MainContainer from '../styles/layout';
 import HomeContent from '../components/Home/HomeContent';
+import totalScoreAtom from '../recoil/atoms';
 
 function Home() {
+  const resetTotalScore = useResetRecoilState(totalScoreAtom);
+
+  useEffect(() => {
+    resetTotalScore();
+  }, []);
+
   return (
     <MainContainer>
       <Logo />
