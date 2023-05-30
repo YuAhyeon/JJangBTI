2f3dc816a96a785665d4d2a6b17f089bb152fa47
youa7878 <dkgus7123@naver.com>
2023-05-30 18:22:37 +0900

    🔨 Add KakaoLoadone - #2

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import GlobalStyle from '../styles/GlobalStyle';
+import KakaoLoadone from '../components/common/KakaoLoadone';
 
 export default function Root() {
   return (
     <>
       <GlobalStyle />
       <Outlet />
+      <KakaoLoadone />
     </>
   );
 }
