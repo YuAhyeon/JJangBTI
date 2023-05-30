95556306080062bf5a46284d0cb51ba8c0c9c396
youa7878 <dkgus7123@naver.com>
2023-05-30 13:21:53 +0900

    🔨 Add Button Click Event - #19

--- src/components/Result/ResultContent.tsx
+++ src/components/Result/ResultContent.tsx
@@ -1,42 +1,9 @@
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useRecoilValue, useResetRecoilState } from 'recoil';
 import Title from '../common/Title';
 import { Container, Subtitle } from '../../styles/shared';
 import buttonCSS from '../common/Button';
-import totalScoreAtom from '../../recoil/atoms';
-import generateMBTI from '../../utils/generateMBTI';
-import getMBTIImage from '../../utils/getMBTIImage';
 
 function ResultContent() {
-  const totalScore = useRecoilValue(totalScoreAtom);
-  const resetTotalScore = useResetRecoilState(totalScoreAtom);
-  const resultMBTI = generateMBTI(totalScore);
-  const image = getMBTIImage(resultMBTI);
-  const navigate = useNavigate();
-
-  const handleRetryTest = () => {
-    resetTotalScore();
-    navigate('/');
-  };
-
-  const copyLink = 'https://github.com/youa7878';
-
-  const handleCopyLink = () => {
-    navigator.clipboard.writeText(copyLink);
-    toast.info('링크가 복사되었습니다.', { autoClose: 2000 });
-  };
-
-  const handleDownloadImage = () => {
-    const link = document.createElement('a');
-    link.href = image;
-    const matchResult = image.match(/[A-Z]{4}/);
-    link.download = matchResult ? matchResult[0] : 'mbti';
-    link.click();
-  };
-
   return (
     <Container>
       <Title />
@@ -45,10 +12,10 @@ function ResultContent() {
         <br />
         짱구는 못말려 캐릭터는?
       </Subtitle>
-      <Image src={image} />
-      <Button onClick={handleDownloadImage}>결과 이미지 저장하기</Button>
-      <Button onClick={handleRetryTest}>테스트 다시하기</Button>
-      <Button onClick={handleCopyLink}>테스트 링크 복사하기</Button>
+      <Image src="https://velog.velcdn.com/images/youa7878/post/f5a163c3-0c91-471f-afd1-9c9f29e2d7ae/image.png" />
+      <Button>결과 이미지 저장하기</Button>
+      <Button>테스트 다시하기</Button>
+      <Button>테스트 링크 복사하기</Button>
     </Container>
   );
 }
