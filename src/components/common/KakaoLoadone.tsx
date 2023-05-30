77f1cb9aae2b69c5d689faa6b7d625e3c720afa4
youa7878 <dkgus7123@naver.com>
2023-05-30 19:56:36 +0900

    🔨 Add env - #17

--- src/components/common/KakaoLoadone.tsx
+++ src/components/common/KakaoLoadone.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 
-function KakaoLoadone() {
+export function KakaoLoadone() {
   const loadKakao = () => {
-    const { VITE_UNIT_ID } = import.meta.env;
+    const { VITE_UNIT_ID_ONE } = import.meta.env;
     const ins = document.createElement('ins');
     const scr = document.createElement('script');
 
@@ -14,7 +14,7 @@ function KakaoLoadone() {
     scr.src = '//t1.daumcdn.net/kas/static/ba.min.js';
     ins.setAttribute('data-ad-width', '320');
     ins.setAttribute('data-ad-height', '100');
-    ins.setAttribute('data-ad-unit', VITE_UNIT_ID);
+    ins.setAttribute('data-ad-unit', VITE_UNIT_ID_ONE);
 
     const adfitElement = document.querySelector('.adfit');
 
@@ -31,4 +31,33 @@ function KakaoLoadone() {
   return <div className="adfit" />;
 }
 
-export default KakaoLoadone;
+export function KakaoLoadtwo() {
+  const loadKakao = () => {
+    const { VITE_UNIT_ID_TWO } = import.meta.env;
+    const ins = document.createElement('ins');
+    const scr = document.createElement('script');
+
+    ins.className = 'kakao_ad_area';
+    ins.style.display = 'none';
+    ins.style.width = '100%';
+    scr.async = true;
+    scr.type = 'text/javascript';
+    scr.src = '//t1.daumcdn.net/kas/static/ba.min.js';
+    ins.setAttribute('data-ad-width', '320');
+    ins.setAttribute('data-ad-height', '100');
+    ins.setAttribute('data-ad-unit', VITE_UNIT_ID_TWO);
+
+    const adfitElement = document.querySelector('.adfit2');
+
+    if (adfitElement) {
+      adfitElement.appendChild(ins);
+      adfitElement.appendChild(scr);
+    }
+  };
+
+  useEffect(() => {
+    loadKakao();
+  }, []);
+
+  return <div className="adfit2" />;
+}
