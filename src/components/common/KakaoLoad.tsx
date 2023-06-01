c353e6558019344fdd7a4fdd00a7fa56378b27eb
youa7878 <dkgus7123@naver.com>
2023-06-01 14:42:36 +0900

    🔨 handleLoadKakao 수정 - #2

--- src/components/common/KakaoLoad.tsx
+++ src/components/common/KakaoLoad.tsx
@@ -1,62 +1,125 @@
 import { useEffect } from 'react';
 
-const handleLoadKakao = (unitID: string) => {
-  const ins = document.createElement('ins');
-  const scr = document.createElement('script');
-
-  ins.className = 'kakao_ad_area';
-  ins.style.display = 'none';
-  ins.style.width = '100%';
-  scr.async = true;
-  scr.type = 'text/javascript';
-  scr.src = '//t1.daumcdn.net/kas/static/ba.min.js';
-  ins.setAttribute('data-ad-width', '320');
-  ins.setAttribute('data-ad-height', '100');
-  ins.setAttribute('data-ad-unit', unitID);
-
-  const adfitElement = document.querySelector('.adfit');
-
-  if (adfitElement) {
-    adfitElement.appendChild(ins);
-    adfitElement.appendChild(scr);
-  }
-};
 export function KakaoLoadOne() {
-  const { VITE_UNIT_ID_ONE } = import.meta.env;
+  const handleLoadKakao = () => {
+    const { VITE_UNIT_ID_ONE } = import.meta.env;
+    const ins = document.createElement('ins');
+    const scr = document.createElement('script');
+
+    ins.className = 'kakao_ad_area';
+    ins.style.display = 'none';
+    ins.style.width = '100%';
+    scr.async = true;
+    scr.type = 'text/javascript';
+    scr.src = '//t1.daumcdn.net/kas/static/ba.min.js';
+    ins.setAttribute('data-ad-width', '320');
+    ins.setAttribute('data-ad-height', '100');
+    ins.setAttribute('data-ad-unit', VITE_UNIT_ID_ONE);
+
+    const adfitElement = document.querySelector('.adfit');
+
+    if (adfitElement) {
+      adfitElement.appendChild(ins);
+      adfitElement.appendChild(scr);
+    }
+  };
 
   useEffect(() => {
-    handleLoadKakao(VITE_UNIT_ID_ONE);
+    handleLoadKakao();
   }, []);
 
   return <div className="adfit" />;
 }
 
 export function KakaoLoadTwo() {
-  const { VITE_UNIT_ID_TWO } = import.meta.env;
+  const handleLoadKakao = () => {
+    const { VITE_UNIT_ID_TWO } = import.meta.env;
+    const ins = document.createElement('ins');
+    const scr = document.createElement('script');
+
+    ins.className = 'kakao_ad_area';
+    ins.style.display = 'none';
+    ins.style.width = '100%';
+    scr.async = true;
+    scr.type = 'text/javascript';
+    scr.src = '//t1.daumcdn.net/kas/static/ba.min.js';
+    ins.setAttribute('data-ad-width', '320');
+    ins.setAttribute('data-ad-height', '100');
+    ins.setAttribute('data-ad-unit', VITE_UNIT_ID_TWO);
+
+    const adfitElement = document.querySelector('.adfit2');
+
+    if (adfitElement) {
+      adfitElement.appendChild(ins);
+      adfitElement.appendChild(scr);
+    }
+  };
 
   useEffect(() => {
-    handleLoadKakao(VITE_UNIT_ID_TWO);
+    handleLoadKakao();
   }, []);
 
-  return <div className="adfit" />;
+  return <div className="adfit2" />;
 }
 
 export function KakaoLoadThree() {
-  const { VITE_UNIT_ID_THREE } = import.meta.env;
+  const handleLoadKakao = () => {
+    const { VITE_UNIT_ID_THREE } = import.meta.env;
+    const ins = document.createElement('ins');
+    const scr = document.createElement('script');
+
+    ins.className = 'kakao_ad_area';
+    ins.style.display = 'none';
+    ins.style.width = '100%';
+    scr.async = true;
+    scr.type = 'text/javascript';
+    scr.src = '//t1.daumcdn.net/kas/static/ba.min.js';
+    ins.setAttribute('data-ad-width', '320');
+    ins.setAttribute('data-ad-height', '100');
+    ins.setAttribute('data-ad-unit', VITE_UNIT_ID_THREE);
+
+    const adfitElement = document.querySelector('.adfit3');
+
+    if (adfitElement) {
+      adfitElement.appendChild(ins);
+      adfitElement.appendChild(scr);
+    }
+  };
 
   useEffect(() => {
-    handleLoadKakao(VITE_UNIT_ID_THREE);
+    handleLoadKakao();
   }, []);
 
-  return <div className="adfit" />;
+  return <div className="adfit3" />;
 }
 
 export function KakaoLoadFour() {
-  const { VITE_UNIT_ID_FOUR } = import.meta.env;
+  const handleLoadKakao = () => {
+    const { VITE_UNIT_ID_FOUR } = import.meta.env;
+    const ins = document.createElement('ins');
+    const scr = document.createElement('script');
+
+    ins.className = 'kakao_ad_area';
+    ins.style.display = 'none';
+    ins.style.width = '100%';
+    scr.async = true;
+    scr.type = 'text/javascript';
+    scr.src = '//t1.daumcdn.net/kas/static/ba.min.js';
+    ins.setAttribute('data-ad-width', '320');
+    ins.setAttribute('data-ad-height', '100');
+    ins.setAttribute('data-ad-unit', VITE_UNIT_ID_FOUR);
+
+    const adfitElement = document.querySelector('.adfit4');
+
+    if (adfitElement) {
+      adfitElement.appendChild(ins);
+      adfitElement.appendChild(scr);
+    }
+  };
 
   useEffect(() => {
-    handleLoadKakao(VITE_UNIT_ID_FOUR);
+    handleLoadKakao();
   }, []);
 
-  return <div className="adfit" />;
+  return <div className="adfit4" />;
 }
